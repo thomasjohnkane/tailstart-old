c23d0a9d5e82b86f29fe8c9c0bbb06e0b38679c7
dylanyp <dylanYP@yahoo.com>
2018-02-08 23:35:37 -0600

    init commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,9 @@
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+var tailwindcss = require('tailwindcss');
+
+mix.js('resources/assets/js/app.js', 'public/js');
+
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind-config.js') ],
+  });
